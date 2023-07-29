df7095ae4fcf04896bb454da53b52b3b75385d75
PROSOFT-E\m.rukhtin <m.rukhtin@prosoftsystems.ru>
2023-07-29 15:42:41 +0500

    Создание вспомогательных методов, повышение "читаемости" кода

--- test_add_group.py
+++ test_add_group.py
@@ -11,9 +11,11 @@ class TestAddGroup(unittest.TestCase):
     def setUp(self):
         self.wd = webdriver.Chrome()
         self.wd.implicitly_wait(30)
-    def test_add_group(self):
-        wd = self.wd
+
+    def open_home_page(self, wd):
         wd.get("http://localhost/addressbook/")
+
+    def login(self, wd):
         wd.find_element(By.NAME, ("user")).click()
         wd.find_element(By.NAME, ("user")).clear()
         wd.find_element(By.NAME, ("user")).send_keys("admin")
@@ -21,8 +23,14 @@ class TestAddGroup(unittest.TestCase):
         wd.find_element(By.NAME, ("pass")).clear()
         wd.find_element(By.NAME, ("pass")).send_keys("secret")
         wd.find_element(By.XPATH, ("//input[@value='Login']")).click()
+    
+    def open_groups_page(self, wd):
         wd.find_element(By.LINK_TEXT, ("groups")).click()
+    
+    def create_group(self, wd):
+        # init group creation
         wd.find_element(By.NAME, ("new")).click()
+        # fill group form
         wd.find_element(By.NAME, ("group_name")).click()
         wd.find_element(By.NAME, ("group_name")).clear()
         wd.find_element(By.NAME, ("group_name")).send_keys("tttetst")
@@ -32,19 +40,23 @@ class TestAddGroup(unittest.TestCase):
         wd.find_element(By.NAME, ("group_footer")).click()
         wd.find_element(By.NAME, ("group_footer")).clear()
         wd.find_element(By.NAME, ("group_footer")).send_keys("gfdgdd")
+        # submit group creation
         wd.find_element(By.NAME, ("submit")).click()
+    
+    def return_to_group_page(self, wd):
         wd.find_element(By.LINK_TEXT, ("group page")).click()
+
+    def logout(self, wd):
         wd.find_element(By.LINK_TEXT, ("Logout")).click()
-    
-    def is_element_present(self, how, what):
-        try: self.wd.find_element(by=how, value=what)
-        except NoSuchElementException as e: return False
-        return True
-    
-    def is_alert_present(self):
-        try: self.wd.switch_to_alert()
-        except NoAlertPresentException as e: return False
-        return True
+
+    def test_add_group(self):
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd)
+        self.open_groups_page(wd)
+        self.create_group(wd)
+        self.return_to_group_page(wd)
+        self.logout(wd)
     
     def tearDown(self):
         self.wd.quit()
