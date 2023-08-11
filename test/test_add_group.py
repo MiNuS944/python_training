a80f90cdb9f1a5798d8a61fec483c75bb655bfee
PROSOFT-E\m.rukhtin <m.rukhtin@prosoftsystems.ru>
2023-08-11 23:03:38 +0500

    Добавлена "интеллектуальность" функций login и logout

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -2,7 +2,7 @@ from model.group import Group
 
 
 def test_add_group(app):    
-    app.group.create(Group(name="tttetst123", header="sasdas", footer="gfdgf"))        
+    app.group.create(Group(name="tttetst123", header="sasdas", footer="gfdgf"))
 
 def test_add_empty_group(app): 
     app.group.create(Group(name="", header="", footer=""))
