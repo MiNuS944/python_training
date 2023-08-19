b0c40a9e4b62630999aa83316746a1e143f9ba64
PROSOFT-E\m.rukhtin <m.rukhtin@prosoftsystems.ru>
2023-08-19 15:48:49 +0500

    Добавлены проверки равенства объектов списка групп, сортировка списков перед сравнением

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -4,15 +4,20 @@ def test_modify_group_name(app):
     if app.group.count() == 0:
         app.group.create(Group(name="tttetst123"))
     old_groups = app.group.get_group_list()
-    app.group.modify_first_group(Group(name="new group"))
+    group = Group(name="new group")
+    group.id = old_groups[0].id
+    app.group.modify_first_group(group)
     new_groups = app.group.get_group_list()
-    assert len(old_groups) == len(new_groups)     
+    assert len(old_groups) == len(new_groups)
+    old_groups[0] = group
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 def test_modify_group_header(app):
     if app.group.count() == 0:
         app.group.create(Group(header="sasdas"))
-    old_groups = app.group.get_group_list()
+    old_groups = app.group.get_group_list() 
     app.group.modify_first_group(Group(header="New header"))
     new_groups = app.group.get_group_list()
-    assert len(old_groups) == len(new_groups)      
+    assert len(old_groups) == len(new_groups)
+       
     
